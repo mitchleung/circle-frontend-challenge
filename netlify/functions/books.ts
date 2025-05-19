7fe44b4a80ed6a0687f812e273658c9c566a8387
Mitch Leung <mitchleung.com@gmail.com>
2025-05-19 18:35:34 +1200

    Fix Netlify books.ts path

--- netlify/functions/books.ts
+++ netlify/functions/books.ts
@@ -1,7 +1,11 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
 import serverless from 'serverless-http'
-import app from '../../src/app'
+import express from 'express';
+import booksRouter from '../../src/router/booksRouter';
 
+const app = express();
+app.use(express.json());
+app.use('/', booksRouter); // match Netlify redirect
 
-export const handler = serverless(app)
+export const handler = serverless(app);
