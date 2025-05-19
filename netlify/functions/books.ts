71a8ab1976b3874e12720405f249edd424691fd7
Mitch Leung <mitchleung.com@gmail.com>
2025-05-19 17:15:47 +1200

    Clone original books api to netlify api

--- netlify/functions/books.ts
+++ netlify/functions/books.ts
@@ -2,12 +2,21 @@
 
 import express, { Router } from 'express'
 import serverless from 'serverless-http'
+import booksRouter from '../../src/router/booksRouter'
+import ErrorHandler from '../../src/utils/ErrorHandler'
+import { errorHandler } from '../../src/middleware/errorHandler'
 
 const api = express()
 
 const router = Router()
 router.get('/hello', (req, res) => res.send('Hello World!'))
 
-api.use('/books', router)
+api.use('/books', booksRouter)
+
+api.use((_req, _res, next) => {
+    next(new ErrorHandler('Route not found', 404))
+})
+
+api.use(errorHandler)
 
 export const handler = serverless(api)
