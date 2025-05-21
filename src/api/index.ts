3171e03595c4495d5c397ee01124b89b7c7f3c72
Mitch Leung <mitchleung.com@gmail.com>
2025-05-22 11:53:12 +1200

    Clone app logic to /api/index

---
+++ src/api/index.ts
@@ -0,0 +1,18 @@
+const express = require('express')
+import app from '../app'
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    console.log(`Local server running on port ${PORT}`)
+})
+
+process.on('unhandledRejection', (reason) => {
+    console.error('Unhandled Rejection', reason)
+})
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception', err)
+})
+
+module.exports = app
