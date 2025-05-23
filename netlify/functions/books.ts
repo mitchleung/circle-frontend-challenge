4d7f558a64e3daae248b6c342d31fe8168b45716
Mitch Leung <mitchleung.com@gmail.com>
2025-05-23 15:57:40 +1200

    update netlify api

--- netlify/functions/books.ts
+++ netlify/functions/books.ts
@@ -3,9 +3,16 @@
 import serverless from 'serverless-http'
 import express from 'express';
 import booksRouter from '../../src/router/booksRouter';
+import app from '../../src';
 
-const app = express();
-app.use(express.json());
-app.use('/books', booksRouter); // match Netlify redirect
+// const app = express();
+// app.use(express.json());
+// app.use('/books', booksRouter); // match Netlify redirect
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    console.log(`Local server running on port ${PORT}`)
+})
 
 export const handler = serverless(app);
